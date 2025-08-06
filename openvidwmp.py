be01211a7ccdf873d9cedb076c3a39aa7d9ba78f
ricki-pierce <rpier12@lsu.edu>
2025-08-06 16:54:24 -0500

    "full screen" and centered on PC

--- openvidwmp.py
+++ openvidwmp.py
@@ -1,55 +1,50 @@
-#Dr Su requested videos be played through Windows Media Player rather than VLC
-# Import Flask so we can create a simple web server on this computer
 from flask import Flask
-
-# These two libraries let us open programs (like videos) and control running apps
 import subprocess
 import os
+import time
+from pywinauto import Application, Desktop
 
-# This line creates the actual web app we’ll use
 app = Flask(__name__)
-
-# This variable will help us remember if Windows Media Player is running
 wmp_process = None
 
-# -------------------------- START VIDEO --------------------------
-
-# This sets up a special web link (called an "endpoint") that, when used, will start a video
-@app.route('/start', methods=['POST'])  # This means we can send a POST request to http://<ip>:5000/start
+@app.route('/start', methods=['POST'])
 def start_wmp():
-    global wmp_process  # So we can update the variable from outside the function too
-
+    global wmp_process
     try:
-        # This is the full location of the video we want to play on the computer
         video_path = r"C:\Users\B24-Lab\Desktop\Videos\1_Ball_Social-.mp4"
 
-        # This tells Windows to open Windows Media Player and play the video
-        # 'start' means open something, 'wmplayer' is Windows Media Player
+        # Launch WMP via subprocess
         wmp_process = subprocess.Popen(
             ['start', 'wmplayer', video_path],
-            shell=True  # 'shell=True' lets us use Windows-style commands
+            shell=True
         )
 
-        return "Windows Media Player started", 200  # Return a success message if all goes well
+        # Give WMP time to launch and load video
+        time.sleep(5)
+
+        # Connect to the WMP window
+        app = Application(backend="uia").connect(title_re=".*Windows Media Player.*")
+        window = app.window(title_re=".*Windows Media Player.*")
+        window.set_focus()
+
+        # Look for "View Full Screen" button
+        full_screen_button = window.child_window(title="View Full Screen", control_type="Button")
+        if full_screen_button.exists():
+            full_screen_button.click_input()
+            return "WMP launched and switched to full screen.", 200
+        else:
+            return "WMP launched, but full screen button not found.", 500
+
     except Exception as e:
-        return f"Error starting Windows Media Player: {e}", 500  # Return an error if something goes wrong
+        return f"Error starting WMP: {e}", 500
 
-# -------------------------- STOP VIDEO --------------------------
-
-# This sets up another link that stops the video playing
-@app.route('/stop', methods=['POST'])  # Send a POST request to http://<ip>:5000/stop to run this
+@app.route('/stop', methods=['POST'])
 def stop_wmp():
     try:
-        # This command forces Windows to shut down Windows Media Player if it's running
         os.system("taskkill /im wmplayer.exe /f")
-        return "Windows Media Player stopped", 200  # If successful, tell the user
+        return "WMP stopped", 200
     except Exception as e:
-        return f"Error stopping Windows Media Player: {e}", 500  # If there's an issue, report it
+        return f"Error stopping WMP: {e}", 500
 
-# -------------------------- START SERVER --------------------------
-
-# This tells Python to actually start the web server when we run this file
-# 'host=0.0.0.0' means anyone on the local network can send commands to it
-# 'port=5000' is just the door number for communication (like a street address + apartment number)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
